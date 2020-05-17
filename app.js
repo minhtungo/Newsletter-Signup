87e767a5e7bea91564fa9f73c25b8db54fabeaff
Minh Tú Ngô <loducfc@gmail.com>
2020-05-16 21:28:06 -0400

    hahaha

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+
+})
+
+app.post('/', function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us18.api.mailchimp.com/3.0/lists/33a1be277e";
+    const options = {
+        method: "POST",
+        auth: "tu:2b12a7767a2a0da1ae000c96b10c108e-us18"
+    }
+    const request = https.request(url, options, function(response){
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.send(__dirname + "/failure.html");
+        }
+        response.on('data',function(data){
+        console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post('/failure',function(req,res){
+    res.redirect("/");
+} )
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Success')
+})
